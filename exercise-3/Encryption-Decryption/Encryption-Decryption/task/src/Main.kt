280c00e07a09ec85a086b07377577483cc0012d0
Dnyaneshwar <dnyaneshwar.ware@walchandsangli.ac.in>
2022-10-23 15:27:49 +0530

    Complete stage 3

--- exercise-3/Encryption-Decryption/Encryption-Decryption/task/src/Main.kt
+++ exercise-3/Encryption-Decryption/Encryption-Decryption/task/src/Main.kt
@@ -1,27 +1,45 @@
 package encryptdecrypt
 
-fun main() {
-
-    var plaintext = readln();
-    var key = readln().toInt();
-
+fun encrypt(plaintext: String, key: Int): String {
     var ciphertext = "";
     for(i in 0 until plaintext.length)
     {
         var current_value = plaintext[i].code - 97;
 
-        if(!(current_value >= 0 && current_value <= 25)){
-            ciphertext += plaintext[i];
-            continue;
-        }
-
-        current_value = (current_value + key) % 26;
+        current_value = (current_value + key);
 
         ciphertext += ((97 + current_value).toChar());
-
     }
+    return ciphertext;
+}
 
-    println(ciphertext)
+fun decrypt(ciphertext: String, key: Int): String {
+    var plaintext = "";
+    for(i in 0 until ciphertext.length)
+    {
+        var current_value = ciphertext[i].code - 97;
 
+        current_value = (current_value - key);
+
+        plaintext += ((97 + current_value).toChar());
+    }
+    return plaintext;
+}
+
+fun main() {
+
+    var operation = readln();
+
+    var text = readln();
+
+    var key = readln().toInt();
+
+    if(operation == "enc"){
+        println(encrypt(text,key));
+    }
+    else
+    {
+        println(decrypt(text,key));
+    }
 
 }
