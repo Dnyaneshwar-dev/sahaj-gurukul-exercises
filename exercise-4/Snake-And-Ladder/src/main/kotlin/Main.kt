955c1e310512f6bebaf70ceeae1ff4d94c024568
dnyaneshwar <dnyaneshwar.ware@walchandsangli.ac.in>
2022-10-24 21:26:09 +0530

    Add main program for snake and ladder

--- exercise-4/Snake-And-Ladder/src/main/kotlin/Main.kt
+++ exercise-4/Snake-And-Ladder/src/main/kotlin/Main.kt
@@ -1,7 +1,118 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.text.FieldPosition
+import java.util.Scanner;
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun move(currentPosition: Int,number: Int, ladders: HashMap<Int,Int>, snakes: HashMap<Int,Int> ): Int  {
+
+    if(currentPosition + number > 100) return currentPosition
+
+    var newPosition:Int = currentPosition + number
+
+    if(snakes.containsKey(newPosition)){
+        newPosition = snakes[newPosition]!!
+    }
+    else if(ladders.containsKey(newPosition))
+    {
+        newPosition = ladders[newPosition]!!
+    }
+
+    return newPosition
+}
+fun main(args: Array<String>) {
+
+    // Variables
+    var ladders: HashMap<Int,Int> = HashMap<Int,Int> ()
+    var snakes: HashMap<Int,Int> = HashMap<Int,Int>()
+
+    // number of ladders
+    var numOfLadders: Int = 0;
+    try {
+        numOfLadders = readln().toInt();
+    }
+    catch (e: Exception)
+    {
+        error("Please input Valid Integer(Number of Ladders)")
+        return
+    }
+
+    // Inputting all ladders
+    var i = 0;
+
+    while (i < numOfLadders){
+        try {
+            var (start, end) = readln().split(" ").map { it.toInt() }
+
+            ladders.put(start,end)
+
+            i += 1
+
+        }
+        catch (e: Exception){
+            error("Wrong Start and End Values!! Enter Valid Numbers");
+        }
+
+    }
+
+
+    // number of snakes
+    var numOfSnakes: Int = 0;
+
+    try {
+        numOfSnakes = readln().toInt()
+    }
+    catch (e:Exception){
+        error("Please input Valid Integer(Number of Snakes)")
+        return
+
+    }
+
+
+    // Inputing all snakes
+    i = 0
+
+    while (i < numOfSnakes){
+        try {
+            var (start, end) = readln().split(" ").map { it.toInt() }
+
+            snakes.put(start,end)
+
+            i += 1
+
+        }
+        catch (e: Exception){
+            error("Wrong Start and End Values!! Enter Valid Numbers");
+        }
+
+    }
+
+
+    // Postion
+    var currentPosition = 1;
+
+
+    // Dice Inputs
+    val scanner = Scanner(System.`in`)
+
+    println("Enter The Sequence of numbers on dice Face (Enter 0 to Stop)")
+
+    while (scanner.hasNext())
+    {
+
+        try {
+            var dice = scanner.nextInt()
+
+            if(dice == 0)
+                break
+
+            currentPosition = move(currentPosition,dice,ladders,snakes)
+        }
+        catch (e: Exception){
+            error("Enter Valid Dice Face")
+        }
+    }
+
+
+    println("Final Position: " + currentPosition)
+    return
+
+
 }
